560262c5594f919ce6c9745595aa83ecab963a22
whatever-shaha <shahasadinov@gmail.com>
2022-08-20 09:35:20 +0500

    initial

---
+++ index.js
@@ -0,0 +1,187 @@
+const groups = [
+    {
+        id: 1,
+        name: 'nice group'
+    },
+    {
+        id: 2,
+        name: 'big group'
+    },
+    {
+        id: 3,
+        name: 'little group'
+    },
+    {
+        id: 4,
+        name: 'beautiful group'
+    }
+]
+
+const users = [
+    {
+        id: 1,
+        name: 'user1',
+        groupId: 1,
+    },
+    {
+        id: 3,
+        name: 'user3',
+        groupId: 1,
+    },
+    {
+        id: 2,
+        name: 'user2',
+        groupId: 1,
+    },
+    {
+        id: 4,
+        name: 'user4',
+        groupId: 1,
+    },
+    {
+        id: 5,
+        name: 'user5',
+        groupId: 2,
+    },
+    {
+        id: 6,
+        name: 'user6',
+        groupId: 2,
+    },
+    {
+        id: 6,
+        name: 'user6',
+        groupId: 2,
+    },
+    {
+        id: 7,
+        name: 'user7',
+        groupId: 2,
+    },
+    {
+        id: 8,
+        name: 'user8',
+        groupId: 2,
+    },
+    {
+        id: 9,
+        name: 'user9',
+        groupId: 2,
+    },
+    {
+        id: 10,
+        name: 'user10',
+        groupId: 3,
+    },
+    {
+        id: 11,
+        name: 'user11',
+        groupId: 3,
+    },
+    {
+        id: 12,
+        name: 'user12',
+        groupId: 3,
+    },
+    {
+        id: 13,
+        name: 'user13',
+        groupId: 3,
+    },
+]
+
+
+const photos = [
+    {
+        id: 1,
+        src: 'src1',
+        userId: 1
+    },
+    {
+        id: 2,
+        src: 'src2',
+        userId: 1
+    },
+    {
+        id: 3,
+        src: 'src3',
+        userId: 2
+    },
+    {
+        id: 4,
+        src: 'src4',
+        userId: 3
+    },
+    {
+        id: 5,
+        src: 'src5',
+        userId: 3
+    },
+    {
+        id: 6,
+        src: 'src6',
+        userId: 4
+    },
+    {
+        id: 7,
+        src: 'src7',
+        userId: 5
+    },
+    {
+        id: 8,
+        src: 'src8',
+        userId: 5
+    },
+    {
+        id: 9,
+        src: 'src9',
+        userId: 5
+    },
+    {
+        id: 10,
+        src: 'src10',
+        userId: 6
+    },
+    {
+        id: 12,
+        src: 'src12',
+        userId: 6
+    },
+    {
+        id: 13,
+        src: 'src13',
+        userId: 6
+    },
+    {
+        id: 14,
+        src: 'src14',
+        userId: 9
+    },
+    {
+        id: 15,
+        src: 'src15',
+        userId: 9
+    },
+    {
+        id: 16,
+        src: 'src16',
+        userId: 9
+    },
+    {
+        id: 17,
+        src: 'src17',
+        userId: 11
+    },
+    {
+        id: 18,
+        src: 'src18',
+        userId: 11
+    },
+    {
+        id: 19,
+        src: 'src19',
+        userId: 13
+    },
+
+
+]
